a020009dda94dd8fd726458f20ac7b01d8f633bc
Pipe Francis <francesco.tubaro@gmail.com>
2024-07-13 19:05:30 +0200

    div table

--- team_rankings_div/team_rankings.js
+++ team_rankings_div/team_rankings.js
@@ -18,12 +18,12 @@ document.addEventListener('DOMContentLoaded', function() {
         card.innerHTML = `
             <div class="team-card-header">${index + 1}. ${team.name}</div>
             <div class="team-card-body">
-                <div><span class="bold">Punteggio:</span> ${team.tot_team}</div>
-                <div class="player-info"><span class="bold">Giocatore 1:</span> ${team.p1.name} (${team.p1.tot})</div>
-                <div class="player-info"><span class="bold">Giocatore 2:</span> ${team.p2.name} (${team.p2.tot})</div>
-                <div class="player-info"><span class="bold">Giocatore 3:</span> ${team.p3.name} (${team.p3.tot})</div>
-                <div class="player-info"><span class="bold">Giocatore 4:</span> ${team.p4.name} (${team.p4.tot})</div>
-                <div class="player-info"><span class="bold">Giocatore 5:</span> ${team.p5.name} (${team.p5.tot})</div>
+                <div><span>Punteggio:</span> <strong>${team.tot_team}</strong></div>
+                <div class="player-info"> ${team.p1.name} (${team.p1.tot})</div>
+                <div class="player-info"> ${team.p2.name} (${team.p2.tot})</div>
+                <div class="player-info"> ${team.p3.name} (${team.p3.tot})</div>
+                <div class="player-info"> ${team.p4.name} (${team.p4.tot})</div>
+                <div class="player-info"> ${team.p5.name} (${team.p5.tot})</div>
                 <div><span class="bold">Rione:</span> ${team.rione.name}</div>
             </div>
         `;
