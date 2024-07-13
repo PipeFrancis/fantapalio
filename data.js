2b22afbd59405da877e99b8e60d7ffacf2e21ed6
Pipe Francis <francesco.tubaro@gmail.com>
2024-07-13 16:15:39 +0200

    aggiornato js per tendina

--- data.js
+++ data.js
@@ -1954,18 +1954,5 @@ export{
         fantateams
     };
 
-// document.addEventListener("DOMContentLoaded", function() {
-//     const select = document.getElementById("teamDetails");
 
-//     // Rimuovi eventuali opzioni esistenti
-//     select.innerHTML = '';
-
-//     // Aggiungi nuove opzioni dal data.js
-//     fantateams.forEach(team => {
-//         const opt = document.createElement('option');
-//         opt.value = team.team_index;
-//         opt.textContent = team.name;
-//         select.appendChild(opt);
-//     });
-// });
 
--- script.js
+++ script.js
@@ -14,20 +14,17 @@ import { fantateams } from './data.js';
 document.addEventListener("DOMContentLoaded", function() {
     const select = document.getElementById("teamDetails");
 
-    // Rimuovi eventuali opzioni esistenti
-    //select.innerHTML = '';
+    //Rimuovi eventuali opzioni esistenti
+    select.innerHTML = '';
 
-    // // Aggiungi nuove opzioni dal data.js
-    // fantateams.forEach(team => {
-    //     const opt = document.createElement('option');
-    //     opt.value = 4;
-    //     opt.textContent = "ciao";
-    //     select.appendChild(opt);
-    // });
-    const opt = document.createElement('option');
-    opt.value = 4;
-    opt.textContent = "ciao";
-    select.appendChild(opt);
+    // Aggiungi nuove opzioni dal data.js
+    fantateams.forEach(team => {
+        const opt = document.createElement('option');
+        opt.value = team.team_index;
+        opt.textContent = team.name;
+        select.appendChild(opt);
+    });
+
 });
 
 
