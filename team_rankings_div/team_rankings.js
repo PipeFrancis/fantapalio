e6e2ff33ce2fa315d46e909cab031574abe1a6c1
Pipe Francis <francesco.tubaro@gmail.com>
2024-07-13 19:09:54 +0200

    div table

--- team_rankings_div/team_rankings.js
+++ team_rankings_div/team_rankings.js
@@ -18,13 +18,13 @@ document.addEventListener('DOMContentLoaded', function() {
         card.innerHTML = `
             <div class="team-card-header">${index + 1}. ${team.name}</div>
             <div class="team-card-body">
-                <div><span>Punteggio:</span> <strong>${team.tot_team}</strong></div>
+                <div class="punteggio-info"><span>Punteggio:</span> <strong>${team.tot_team}</strong></div>
                 <div class="player-info"> ${team.p1.name} (${team.p1.tot})</div>
                 <div class="player-info"> ${team.p2.name} (${team.p2.tot})</div>
                 <div class="player-info"> ${team.p3.name} (${team.p3.tot})</div>
                 <div class="player-info"> ${team.p4.name} (${team.p4.tot})</div>
                 <div class="player-info"> ${team.p5.name} (${team.p5.tot})</div>
-                <div>Rione: <strong>${team.rione.name}</strong></div>
+                <div>${team.rione.name}</div>
             </div>
         `;
         teamCardsContainer.appendChild(card);
