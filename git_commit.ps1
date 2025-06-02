a831601d7fd86550be19cac0c6659e6125630e76
paliocodroipo <francesco.tubaro.whatsapp>
2025-06-02 22:20:22 +0200

    added date print on bash script

--- git_commit.ps1
+++ git_commit.ps1
@@ -7,6 +7,10 @@ git add .
 # Show the git status
 git status
 
+# Print the current date and time with seconds and a new line
+Write-Output "Current Date and Time: $(Get-Date -Format "yyyy-MM-dd HH:mm:ss")"
+Write-Output ""  # This prints a blank line for spacing
+
 # Prompt for commit message
 $commitMessage = Read-Host "Enter commit message"
 
