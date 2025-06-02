84c61d9cf5b7e84c93a26942d7b5a154d4e2c3fb
paliocodroipo <francesco.tubaro.whatsapp>
2025-06-02 22:21:32 +0200

    modified to have timestamp after commit msg

--- git_commit.ps1
+++ git_commit.ps1
@@ -7,12 +7,14 @@ git add .
 # Show the git status
 git status
 
-# Print the current date and time with seconds and a new line
-Write-Output "Current Date and Time: $(Get-Date -Format "yyyy-MM-dd HH:mm:ss")"
-Write-Output ""  # This prints a blank line for spacing
-
 # Prompt for commit message
 $commitMessage = Read-Host "Enter commit message"
+
+
+Write-Output ""  # This prints a blank line for spacing
+# Print the current date and time with seconds
+Write-Output "Current Date and Time: $(Get-Date -Format "yyyy-MM-dd HH:mm:ss")" 
+Write-Output ""  # This prints a blank line for spacing
 
 # Commit with the provided message
 git commit -m "$commitMessage"
