1e433c892178782592a48acbb8ec0ef93fb6159b
PipeFrancis <francesco.tubaro@gmail.com>
2025-06-13 01:40:48 +0200

    ordered rioni in coppachiosco

--- coppachiosco/coppachiosco.js
+++ coppachiosco/coppachiosco.js
@@ -3,9 +3,12 @@ import { rioni25 } from '../data250613_0138.js';
 window.addEventListener('DOMContentLoaded', () => {
     const container = document.querySelector('.team-cards-container1');
 
-    rioni25.forEach(rione => {
+    // Sort the rioni25 array in descending order by chiosco_tot
+    const sortedRioni = [...rioni25].sort((a, b) => b.chiosco_tot - a.chiosco_tot);
+
+    sortedRioni.forEach(rione => {
         const card = document.createElement('div');
-        card.classList.add('team-card1',`cardclass${rione.name}`);
+        card.classList.add('team-card1', `cardclass${rione.name}`);
 
         card.innerHTML = `
             <h2>${rione.name}</h2>
