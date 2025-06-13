6e6a6386466ceac420cf770b5ff5eb130a73b2b4
PipeFrancis <francesco.tubaro@gmail.com>
2025-06-13 22:43:35 +0200

    headerlink right

--- player_rankings_div/player_rankings.js
+++ player_rankings_div/player_rankings.js
@@ -1,7 +1,7 @@
 import {
     player_type,
     players
-} from '../data250613_2210.js';
+} from '../data250613_2243.js';
 
 document.addEventListener('DOMContentLoaded', function() {
     const playerCardsContainer = document.getElementById('playerCardsContainer');
--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-import { fantateams } from './data250613_2210.js';
+import { fantateams } from './data250613_2243.js';
 
 document.addEventListener("DOMContentLoaded", function() {
     const select = document.getElementById("teamDetails");
