6441e6a4f8a5bf96ba56330fcd6d249bb4c0a3ab
PipeFrancis <francesco.tubaro@gmail.com>
2025-06-13 21:20:36 +0200

    compressed image update version css

--- team_rankings_div/team_rankings.js
+++ team_rankings_div/team_rankings.js
@@ -2,7 +2,7 @@ import {
     fantateam_type,
     NORD, SUD, EST, WEST,
     fantateams
-} from '../data250613_1129.js';
+} from '../data250613_2120.js';
 
 document.addEventListener('DOMContentLoaded', function() {
     const teamCardsContainer = document.getElementById('teamCardsContainer');
