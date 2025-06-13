1084388c18b88dc0593cdc5ab1eed8f689363143
PipeFrancis <francesco.tubaro@gmail.com>
2025-06-13 23:45:42 +0200

    bigger

--- meme_p_rankings/meme_p_rankings.js
+++ meme_p_rankings/meme_p_rankings.js
@@ -3,7 +3,7 @@ import {
     player_type,
     players,
     td3Weights
-} from '../data250613_2344.js';
+} from '../data250613_2346.js';
 
 document.addEventListener('DOMContentLoaded', function() {
     const playerCardsContainer = document.getElementById('playerCardsContainer');
--- player_rankings_div/player_rankings.js
+++ player_rankings_div/player_rankings.js
@@ -1,7 +1,7 @@
 import {
     player_type,
     players
-} from '../data250613_2344.js';
+} from '../data250613_2346.js';
 
 document.addEventListener('DOMContentLoaded', function() {
     const playerCardsContainer = document.getElementById('playerCardsContainer');
