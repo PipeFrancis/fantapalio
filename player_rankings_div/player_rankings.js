56f1937dee21cceb59b10320ceaf542fb2ce5ee8
PipeFrancis <francesco.tubaro@gmail.com>
2025-06-29 17:29:44 +0200

    put in stats from gsheet with script

--- player_rankings_div/player_rankings.js
+++ player_rankings_div/player_rankings.js
@@ -1,7 +1,7 @@
 import {
     player_type,
     players
-} from '../data250629_1607.js';
+} from '../data250629_1729.js';
 
 document.addEventListener('DOMContentLoaded', function() {
     const playerCardsContainer = document.getElementById('playerCardsContainer');
--- registration/registration.js
+++ registration/registration.js
@@ -1,4 +1,4 @@
-import { players } from '../data250629_1607.js';
+import { players } from '../data250629_1729.js';
 console.log("inizio registration.js");
 
 // Group players by their team
