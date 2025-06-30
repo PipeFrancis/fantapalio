e842608efa2e0c00e7cf274134b72dee2344f715
PipeFrancis <francesco.tubaro@gmail.com>
2025-06-30 21:32:52 +0200

    reverse button colors

--- coppachiosco/coppachiosco.js
+++ coppachiosco/coppachiosco.js
@@ -1,4 +1,4 @@
-import { rioni25 } from '../data250630_2130.js';
+import { rioni25 } from '../data250630_2132.js';
 
 window.addEventListener('DOMContentLoaded', () => {
     const container = document.querySelector('.team-cards-container1');
--- version_prepare.py
+++ version_prepare.py
@@ -4,8 +4,8 @@ import shutil
 #                  DA CAMBIARE QUESTE 2
 
 # Constants for old and new timestamps
-OLD_TIMESTAMP = "250630_2128"  # replace with the old timestamp
-NEW_TIMESTAMP = "250630_2130"  # replace with the new timestamp
+OLD_TIMESTAMP = "250630_2130"  # replace with the old timestamp
+NEW_TIMESTAMP = "250630_2132"  # update timestamp
 #################################################################################
 
 # List of files to update (add any .html, .js, etc. you need)
