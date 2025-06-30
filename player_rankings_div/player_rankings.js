ca7eb4fa89b2d2766621e9a888ec46e84d4cec3e
PipeFrancis <francesco.tubaro@gmail.com>
2025-06-30 21:35:50 +0200

    button colors

--- player_rankings_div/player_rankings.js
+++ player_rankings_div/player_rankings.js
@@ -1,7 +1,7 @@
 import {
     player_type,
     players
-} from '../data250630_2132.js';
+} from '../data250630_2136.js';
 
 document.addEventListener('DOMContentLoaded', function() {
     const playerCardsContainer = document.getElementById('playerCardsContainer');
--- registration/registration.js
+++ registration/registration.js
@@ -1,4 +1,4 @@
-import { players } from '../data250630_2132.js';
+import { players } from '../data250630_2136.js';
 console.log("inizio registration.js");
 
 // Group players by their team
