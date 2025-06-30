eaeb3b9ffa53bdea9624a39bf177db08de1c0306
PipeFrancis <francesco.tubaro@gmail.com>
2025-06-30 20:43:17 +0200

    avviso bgcolor arancione

--- player_rankings_div/player_rankings.js
+++ player_rankings_div/player_rankings.js
@@ -1,7 +1,7 @@
 import {
     player_type,
     players
-} from '../data250630_2037.js';
+} from '../data250630_2043.js';
 
 document.addEventListener('DOMContentLoaded', function() {
     const playerCardsContainer = document.getElementById('playerCardsContainer');
