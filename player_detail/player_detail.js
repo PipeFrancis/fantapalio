a8065ca6f4ee270f841917bcdf81f02883b36f5c
PipeFrancis <francesco.tubaro@gmail.com>
2025-06-30 20:50:01 +0200

    smaller image

--- player_detail/player_detail.js
+++ player_detail/player_detail.js
@@ -3,7 +3,7 @@
 // se si cambiano array di stats è da cambiare anche questo codice
 
 
-import { players, pdkWeights, td3Weights } from '../data250630_2048.js';
+import { players, pdkWeights, td3Weights } from '../data250630_2049.js';
 
 document.addEventListener("DOMContentLoaded", function() {
     const select = document.getElementById("playerDetails");
--- version_prepare.py
+++ version_prepare.py
@@ -4,8 +4,8 @@ import shutil
 #                  DA CAMBIARE QUESTE 2
 
 # Constants for old and new timestamps
-OLD_TIMESTAMP = "250630_2043"  # replace with the old timestamp
-NEW_TIMESTAMP = "250630_2048"  # replace with the new timestamp
+OLD_TIMESTAMP = "250630_2048"  # replace with the old timestamp
+NEW_TIMESTAMP = "250630_2049"  # replace with the new timestamp
 #################################################################################
 
 # List of files to update (add any .html, .js, etc. you need)
