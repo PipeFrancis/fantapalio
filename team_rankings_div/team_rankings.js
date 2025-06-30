d5c9f2c4239360f09ff4a5168ea090542f2504c5
PipeFrancis <francesco.tubaro@gmail.com>
2025-06-30 21:29:45 +0200

    agnoluzzi sud orange titles

--- team_rankings_div/team_rankings.js
+++ team_rankings_div/team_rankings.js
@@ -2,7 +2,7 @@ import {
     fantateam_type,
     NORD, SUD, EST, WEST,
     fantateams
-} from '../data250630_2115.js';
+} from '../data250630_2128.js';
 
 document.addEventListener('DOMContentLoaded', function() {
     const teamCardsContainer = document.getElementById('teamCardsContainer');
