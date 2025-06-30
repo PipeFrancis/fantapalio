be111272210a0c6fd9218e9e4075d6a5eb595da8
PipeFrancis <francesco.tubaro@gmail.com>
2025-06-30 22:57:08 +0200

    teams2025

--- coppachiosco/coppachiosco.js
+++ coppachiosco/coppachiosco.js
@@ -1,4 +1,4 @@
-import { rioni25 } from '../data250630_2232.js';
+import { rioni25 } from '../data250630_2256.js';
 
 window.addEventListener('DOMContentLoaded', () => {
     const container = document.querySelector('.team-cards-container1');
--- version_prepare.py
+++ version_prepare.py
@@ -4,8 +4,8 @@ import shutil
 #                  DA CAMBIARE QUESTE 2
 
 # Constants for old and new timestamps
-OLD_TIMESTAMP = "250630_2208"  # replace with the old timestamp
-NEW_TIMESTAMP = "250630_2232"  # update timestamp
+OLD_TIMESTAMP = "250630_2232"  # replace with the old timestamp
+NEW_TIMESTAMP = "250630_2256"  # update timestamp
 #################################################################################
 
 # List of files to update (add any .html, .js, etc. you need)
