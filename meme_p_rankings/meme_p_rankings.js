d26a90c1849125648a350d2362f1bb780b2b964d
PipeFrancis <francesco.tubaro@gmail.com>
2025-06-30 21:40:51 +0200

    thicker border

--- meme_p_rankings/meme_p_rankings.js
+++ meme_p_rankings/meme_p_rankings.js
@@ -3,7 +3,7 @@ import {
     player_type,
     players,
     td3Weights
-} from '../data250630_2140.js';
+} from '../data250630_2141.js';
 
 document.addEventListener('DOMContentLoaded', function() {
     const playerCardsContainer = document.getElementById('playerCardsContainer');
--- registration/registration.js
+++ registration/registration.js
@@ -1,4 +1,4 @@
-import { players } from '../data250630_2140.js';
+import { players } from '../data250630_2141.js';
 console.log("inizio registration.js");
 
 // Group players by their team
