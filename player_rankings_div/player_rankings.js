ba72d10c41e7e57a033f45937aa9f260dedb5b13
PipeFrancis <francesco.tubaro@gmail.com>
2025-06-30 22:03:01 +0200

    instabuttonborder

--- player_rankings_div/player_rankings.js
+++ player_rankings_div/player_rankings.js
@@ -1,7 +1,7 @@
 import {
     player_type,
     players
-} from '../data250630_2202.js';
+} from '../data250630_2203.js';
 
 document.addEventListener('DOMContentLoaded', function() {
     const playerCardsContainer = document.getElementById('playerCardsContainer');
--- team_rankings_div/team_rankings.js
+++ team_rankings_div/team_rankings.js
@@ -2,7 +2,7 @@ import {
     fantateam_type,
     NORD, SUD, EST, WEST,
     fantateams
-} from '../data250630_2202.js';
+} from '../data250630_2203.js';
 
 document.addEventListener('DOMContentLoaded', function() {
     const teamCardsContainer = document.getElementById('teamCardsContainer');
