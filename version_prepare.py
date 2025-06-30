92bc2e37a29347879ce81c3024a64b3a8ee04e04
PipeFrancis <francesco.tubaro@gmail.com>
2025-06-30 21:44:35 +0200

    style

--- version_prepare.py
+++ version_prepare.py
@@ -4,8 +4,8 @@ import shutil
 #                  DA CAMBIARE QUESTE 2
 
 # Constants for old and new timestamps
-OLD_TIMESTAMP = "250630_2140"  # replace with the old timestamp
-NEW_TIMESTAMP = "250630_2141"  # update timestamp
+OLD_TIMESTAMP = "250630_2141"  # replace with the old timestamp
+NEW_TIMESTAMP = "250630_2144"  # update timestamp
 #################################################################################
 
 # List of files to update (add any .html, .js, etc. you need)
