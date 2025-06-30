813e049d01e9cb396f8f43392eb5f107977532a6
PipeFrancis <francesco.tubaro@gmail.com>
2025-06-30 21:39:44 +0200

    border buttons

--- coppachiosco/coppachiosco.js
+++ coppachiosco/coppachiosco.js
@@ -1,4 +1,4 @@
-import { rioni25 } from '../data250630_2139.js';
+import { rioni25 } from '../data250630_2140.js';
 
 window.addEventListener('DOMContentLoaded', () => {
     const container = document.querySelector('.team-cards-container1');
--- player_rankings_div/player_rankings.js
+++ player_rankings_div/player_rankings.js
@@ -1,7 +1,7 @@
 import {
     player_type,
     players
-} from '../data250630_2139.js';
+} from '../data250630_2140.js';
 
 document.addEventListener('DOMContentLoaded', function() {
     const playerCardsContainer = document.getElementById('playerCardsContainer');
