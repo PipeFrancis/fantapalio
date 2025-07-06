bac09d98742718dfd7defb6938e3d530ea9cc5d1
PipeFrancis <francesco.tubaro@gmail.com>
2025-07-06 20:54:06 +0200

    round decimal 01 and 51

--- team_rankings_div/team_rankings.js
+++ team_rankings_div/team_rankings.js
@@ -2,7 +2,7 @@ import {
     fantateam_type,
     NORD, SUD, EST, WEST,
     fantateams
-} from '../data250706_2039.js';
+} from '../data250706_2054.js';
 
 document.addEventListener('DOMContentLoaded', function() {
     const teamCardsContainer = document.getElementById('teamCardsContainer');
