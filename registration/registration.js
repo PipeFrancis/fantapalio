6a430b05184c773b9bd250ff93ff35826576b5cf
PipeFrancis <francesco.tubaro@gmail.com>
2025-07-06 20:35:23 +0200

    debug avg

--- registration/registration.js
+++ registration/registration.js
@@ -1,4 +1,4 @@
-import { players } from '../data250706_2028.js';
+import { players } from '../data250706_2035.js';
 console.log("inizio registration.js");
 
 // Group players by their team
--- version_prepare.py
+++ version_prepare.py
@@ -4,8 +4,8 @@ import shutil
 #                  DA CAMBIARE QUESTE 2
 
 # Constants for old and new timestamps
-OLD_TIMESTAMP = "250706_2016"  # replace with the old timestamp
-NEW_TIMESTAMP = "250706_2028"  # update timestamp
+OLD_TIMESTAMP = "250706_2028"  # replace with the old timestamp
+NEW_TIMESTAMP = "250706_2035"  # update timestamp
 #################################################################################
 
 # List of files to update (add any .html, .js, etc. you need)
