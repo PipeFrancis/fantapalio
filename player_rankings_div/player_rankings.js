3015a60d5f809649124515187cd8c0794db70655
PipeFrancis <francesco.tubaro@gmail.com>
2025-07-08 19:56:23 +0200

    new stats and numero maglia

--- player_rankings_div/player_rankings.js
+++ player_rankings_div/player_rankings.js
@@ -1,7 +1,7 @@
 import {
     player_type,
     players
-} from '../data250708_0011.js';
+} from '../data250708_1956.js';
 
 document.addEventListener('DOMContentLoaded', function() {
     const playerCardsContainer = document.getElementById('playerCardsContainer');
--- t_rankings_by_game/t_rankings_by_game.js
+++ t_rankings_by_game/t_rankings_by_game.js
@@ -2,7 +2,7 @@ import {
     fantateam_type,
     NORD, SUD, EST, WEST,
     fantateams
-} from '../data250708_0011.js';
+} from '../data250708_1956.js';
 
 document.addEventListener('DOMContentLoaded', function() {
     const teamCardsContainer = document.getElementById('teamCardsContainer');
