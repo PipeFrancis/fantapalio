8a4aeca4830172f86705a1400b74168a295a9822
PipeFrancis <francesco.tubaro@gmail.com>
2025-07-10 00:55:03 +0200

    timestamp fix

--- coppachiosco/coppachiosco.js
+++ coppachiosco/coppachiosco.js
@@ -1,4 +1,4 @@
-import { rioni25 } from '../data250710_0052.js';
+import { rioni25 } from '../data250710_0055.js';
 
 window.addEventListener('DOMContentLoaded', () => {
     const container = document.querySelector('.team-cards-container1');
