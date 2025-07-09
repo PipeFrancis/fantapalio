3948ac9390ad11e6a632be1e8c2476f70fbc3002
PipeFrancis <francesco.tubaro@gmail.com>
2025-07-09 23:41:40 +0200

    teams 0907 2330, smaller maybe text in teamsimulation

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-import { fantateams } from './data250708_2311.js';
+import { fantateams } from './data250709_2340.js';
 
 document.addEventListener("DOMContentLoaded", function() {
     const select = document.getElementById("teamDetails");
