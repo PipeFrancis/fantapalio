8af6e949dc937a2d7756429dc96adb473b352661
PipeFrancis <francesco.tubaro@gmail.com>
2025-07-11 13:55:35 +0200

    try put some memes in player with no final appearence

--- coppachiosco/coppachiosco.js
+++ coppachiosco/coppachiosco.js
@@ -1,4 +1,4 @@
-import { rioni25 } from '../data250711_1353.js';
+import { rioni25 } from '../data250711_1355.js';
 
 window.addEventListener('DOMContentLoaded', () => {
     const container = document.querySelector('.team-cards-container1');
--- player_rankings_div/player_rankings.js
+++ player_rankings_div/player_rankings.js
@@ -1,7 +1,7 @@
 import {
     player_type,
     players
-} from '../data250711_1353.js';
+} from '../data250711_1355.js';
 
 document.addEventListener('DOMContentLoaded', function() {
     const playerCardsContainer = document.getElementById('playerCardsContainer');
