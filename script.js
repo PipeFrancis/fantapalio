fb6b5da0b0d211e408b1aa9bcc6157952b67e682
PipeFrancis <francesco.tubaro@gmail.com>
2025-07-11 14:05:57 +0200

    end fix with flag, seems to work

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-import { fantateams } from './data250711_1403.js';
+import { fantateams } from './data250711_1405.js';
 
 document.addEventListener("DOMContentLoaded", function() {
     const select = document.getElementById("teamDetails");
