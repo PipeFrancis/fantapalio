b41d52e669e73b1be3e60399106787f74c50e4d8
PipeFrancis <francesco.tubaro@gmail.com>
2025-07-11 20:56:33 +0200

    no prezo

--- team_rankings_div/team_rankings.js
+++ team_rankings_div/team_rankings.js
@@ -2,7 +2,7 @@ import {
     fantateam_type,
     NORD, SUD, EST, WEST,
     fantateams
-} from '../data250711_2052.js';
+} from '../data250711_2056.js';
 
 document.addEventListener('DOMContentLoaded', function() {
     const teamCardsContainer = document.getElementById('teamCardsContainer');
--- version_prepare.py
+++ version_prepare.py
@@ -4,8 +4,8 @@ import shutil
 #                  DA CAMBIARE QUESTE 2
 
 # Constants for old and new timestamps
-OLD_TIMESTAMP = "250711_2050"  # replace with the old timestamp
-NEW_TIMESTAMP = "250711_2052"  # update timestamp
+OLD_TIMESTAMP = "250711_2052"  # replace with the old timestamp
+NEW_TIMESTAMP = "250711_2056"  # update timestamp
 #################################################################################
 
 # List of files to update (add any .html, .js, etc. you need)
