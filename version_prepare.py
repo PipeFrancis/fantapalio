2d0f0c4660e8f2062685be6dbd839f163c8dec2a
PipeFrancis <francesco.tubaro@gmail.com>
2025-07-12 18:43:52 +0200

    ft100 was duplicated

--- version_prepare.py
+++ version_prepare.py
@@ -4,8 +4,8 @@ import shutil
 #                  DA CAMBIARE QUESTE 2
 
 # Constants for old and new timestamps
-OLD_TIMESTAMP = "250712_1731"  # replace with the old timestamp
-NEW_TIMESTAMP = "250712_1811"  # update timestamp
+OLD_TIMESTAMP = "250712_1811"  # replace with the old timestamp
+NEW_TIMESTAMP = "250712_1844"  # update timestamp
 #################################################################################
 
 # List of files to update (add any .html, .js, etc. you need)
