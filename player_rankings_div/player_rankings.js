d1b7ad04a0beb8484b8d916d40f7089437d55dc2
PipeFrancis <francesco.tubaro@gmail.com>
2025-07-12 16:59:12 +0200

    now should work

--- player_rankings_div/player_rankings.js
+++ player_rankings_div/player_rankings.js
@@ -2,7 +2,7 @@ import {
     player_type,
     players,
     what_day_is_it
-} from '../data250712_1657.js';
+} from '../data250712_1659.js';
 
 document.addEventListener('DOMContentLoaded', function() {
     const playerCardsContainer = document.getElementById('playerCardsContainer');
--- t_rankings_by_game/t_rankings_by_game.js
+++ t_rankings_by_game/t_rankings_by_game.js
@@ -2,7 +2,7 @@ import {
     fantateam_type,
     NORD, SUD, EST, WEST,
     fantateams
-} from '../data250712_1657.js';
+} from '../data250712_1659.js';
 
 document.addEventListener('DOMContentLoaded', function() {
     const teamCardsContainer = document.getElementById('teamCardsContainer');
