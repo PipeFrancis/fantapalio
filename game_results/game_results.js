fd0431d23ade446229c7a3a5ad75598b77f20bef
PipeFrancis <francesco.tubaro@gmail.com>
2025-07-13 18:30:50 +0200

    fix t rank timestamp

--- game_results/game_results.js
+++ game_results/game_results.js
@@ -1,6 +1,6 @@
 import {
   game_results_array   
-} from '../data250713_1817.js';
+} from '../data250713_1831.js';
 
 const teamClassMap = {
   'SUD': 'score_sud',
--- registration/registration.js
+++ registration/registration.js
@@ -1,4 +1,4 @@
-import { players } from '../data250713_1817.js';
+import { players } from '../data250713_1831.js';
 console.log("inizio registration.js");
 
 // Group players by their team
