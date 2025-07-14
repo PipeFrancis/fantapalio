b5afb3b3c2375b4457060567f5c0f22bdd19b9d4
PipeFrancis <francesco.tubaro@gmail.com>
2025-07-14 13:42:50 +0200

    stats quasi ultime domenica

--- coppachiosco/coppachiosco.js
+++ coppachiosco/coppachiosco.js
@@ -1,4 +1,4 @@
-import { rioni25 } from '../data250714_0904.js';
+import { rioni25 } from '../data250714_1342.js';
 
 window.addEventListener('DOMContentLoaded', () => {
     const container = document.querySelector('.team_detail_team-cards-container1');
--- team_rankings_div/team_rankings.js
+++ team_rankings_div/team_rankings.js
@@ -2,7 +2,7 @@ import {
     fantateam_type,
     NORD, SUD, EST, WEST,
     fantateams
-} from '../data250714_0904.js';
+} from '../data250714_1342.js';
 
 document.addEventListener('DOMContentLoaded', function() {
     const teamCardsContainer = document.getElementById('teamCardsContainer');
