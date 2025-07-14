66beb34b394a2d23597e65aa2e42ea2ab9be6a5f
PipeFrancis <francesco.tubaro@gmail.com>
2025-07-14 19:01:28 +0200

    clone off site

--- player_rankings_div/player_rankings.js
+++ player_rankings_div/player_rankings.js
@@ -2,7 +2,7 @@ import {
     player_type,
     players,
     what_day_is_it
-} from '../data250714_1418.js';
+} from '../data250714_1901.js';
 
 document.addEventListener('DOMContentLoaded', function() {
     const playerCardsContainer = document.getElementById('playerCardsContainer');
--- team_rankings_div/team_rankings.js
+++ team_rankings_div/team_rankings.js
@@ -2,7 +2,7 @@ import {
     fantateam_type,
     NORD, SUD, EST, WEST,
     fantateams
-} from '../data250714_1418.js';
+} from '../data250714_1901.js';
 
 document.addEventListener('DOMContentLoaded', function() {
     const teamCardsContainer = document.getElementById('teamCardsContainer');
