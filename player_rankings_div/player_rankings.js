7abd5309113b9cde44db0ef79ce79c63fd2ac913
PipeFrancis <francesco.tubaro@gmail.com>
2025-07-14 02:30:43 +0200

    nobile 1 FTx not 8

--- player_rankings_div/player_rankings.js
+++ player_rankings_div/player_rankings.js
@@ -2,7 +2,7 @@ import {
     player_type,
     players,
     what_day_is_it
-} from '../data250714_0225.js';
+} from '../data250714_0230.js';
 
 document.addEventListener('DOMContentLoaded', function() {
     const playerCardsContainer = document.getElementById('playerCardsContainer');
