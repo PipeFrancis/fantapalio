b8277549cf6e826b93baf402cc267e2737568250
PipeFrancis <francesco.tubaro@gmail.com>
2025-07-14 08:50:28 +0200

    no box score avg 24

--- game_results/game_results.js
+++ game_results/game_results.js
@@ -1,6 +1,6 @@
 import {
   game_results_array   
-} from '../data250714_0839.js';
+} from '../data250714_0850.js';
 
 const teamClassMap = {
   'SUD': 'score_sud',
--- t_rankings_by_game/t_rankings_by_game.js
+++ t_rankings_by_game/t_rankings_by_game.js
@@ -2,7 +2,7 @@ import {
     fantateam_type,
     NORD, SUD, EST, WEST,
     fantateams
-} from '../data250714_0839.js';
+} from '../data250714_0850.js';
 
 document.addEventListener('DOMContentLoaded', function() {
     const teamCardsContainer = document.getElementById('teamCardsContainer');
