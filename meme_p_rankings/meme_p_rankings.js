317dfaf1ca11068bc2ea2ee6760afe79ccf87531
PipeFrancis <francesco.tubaro@gmail.com>
2025-07-16 19:24:04 +0200

    90% width

--- meme_p_rankings/meme_p_rankings.js
+++ meme_p_rankings/meme_p_rankings.js
@@ -4,7 +4,7 @@ import {
     players,
     td3Weights,
     what_day_is_it
-} from '../data250716_1921.js';
+} from '../data250716_1924.js';
 
 document.addEventListener('DOMContentLoaded', function() {
     const playerCardsContainer = document.getElementById('playerCardsContainer');
--- player_rankings_div/player_rankings.js
+++ player_rankings_div/player_rankings.js
@@ -2,7 +2,7 @@ import {
     player_type,
     players,
     what_day_is_it
-} from '../data250716_1921.js';
+} from '../data250716_1924.js';
 
 document.addEventListener('DOMContentLoaded', function() {
     const playerCardsContainer = document.getElementById('playerCardsContainer');
