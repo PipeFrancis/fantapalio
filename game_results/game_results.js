4b8f5e9e2f1454458af46638cf9a471540fbca30
PipeFrancis <francesco.tubaro@gmail.com>
2025-07-17 01:41:20 +0200

    pietro soramel 3 pt meme g3

--- game_results/game_results.js
+++ game_results/game_results.js
@@ -1,6 +1,6 @@
 import {
   game_results_array   
-} from '../data250717_0135.js';
+} from '../data250717_0141.js';
 
 const teamClassMap = {
   'SUD': 'score_sud',
--- version_prepare.py
+++ version_prepare.py
@@ -4,8 +4,8 @@ import shutil
 #                  DA CAMBIARE QUESTE 2
 
 # Constants for old and new timestamps
-OLD_TIMESTAMP = "250717_0130"  # replace with the old timestamp
-NEW_TIMESTAMP = "250717_0135"  # update timestamp
+OLD_TIMESTAMP = "250717_0135"  # replace with the old timestamp
+NEW_TIMESTAMP = "250717_0141"  # update timestamp
 #################################################################################
 
 # List of files to update (add any .html, .js, etc. you need)
