e582fea62b9a28655ab2db7b8cc8d5369ddc0aa9
PipeFrancis <francesco.tubaro@gmail.com>
2025-07-17 02:01:34 +0200

    interviste

--- coppachiosco/coppachiosco.js
+++ coppachiosco/coppachiosco.js
@@ -1,4 +1,4 @@
-import { rioni25 } from '../data250717_0151.js';
+import { rioni25 } from '../data250717_0201.js';
 
 window.addEventListener('DOMContentLoaded', () => {
     const container = document.querySelector('.team_detail_team-cards-container1');
--- player_rankings_div/player_rankings.js
+++ player_rankings_div/player_rankings.js
@@ -2,7 +2,7 @@ import {
     player_type,
     players,
     what_day_is_it
-} from '../data250717_0151.js';
+} from '../data250717_0201.js';
 
 document.addEventListener('DOMContentLoaded', function() {
     const playerCardsContainer = document.getElementById('playerCardsContainer');
