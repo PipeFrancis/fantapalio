b028026b5fa0c8cdd87f9112a1b36ffff2882231
PipeFrancis <francesco.tubaro@gmail.com>
2025-07-19 14:49:47 +0200

    puntimedi in

--- registration/registration.js
+++ registration/registration.js
@@ -1,4 +1,4 @@
-import { players } from '../data250719_1347.js';
+import { players } from '../data250719_1449.js';
 console.log("inizio registration.js");
 
 // Group players by their team
--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-import { fantateams , what_day_is_it} from './data250719_1347.js';
+import { fantateams , what_day_is_it} from './data250719_1449.js';
 
 document.addEventListener("DOMContentLoaded", function() {
     const select = document.getElementById("teamDetails");
