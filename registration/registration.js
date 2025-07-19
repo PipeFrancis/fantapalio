54904839090dac708159fac774ceffb3ccecd590
PipeFrancis <francesco.tubaro@gmail.com>
2025-07-19 14:52:21 +0200

    classifica bonus e chiosco

--- registration/registration.js
+++ registration/registration.js
@@ -1,4 +1,4 @@
-import { players } from '../data250719_1449.js';
+import { players } from '../data250719_1451.js';
 console.log("inizio registration.js");
 
 // Group players by their team
--- version_prepare.py
+++ version_prepare.py
@@ -4,8 +4,8 @@ import shutil
 #                  DA CAMBIARE QUESTE 2
 
 # Constants for old and new timestamps
-OLD_TIMESTAMP = "250719_1347"  # replace with the old timestamp
-NEW_TIMESTAMP = "250719_1449"  # update timestamp
+OLD_TIMESTAMP = "250719_1449"  # replace with the old timestamp
+NEW_TIMESTAMP = "250719_1451"  # update timestamp
 #################################################################################
 
 # List of files to update (add any .html, .js, etc. you need)
