ff74a05ed323772c3c3b40f00508321a02c5039a
pipefrancis <francesco.tubaro@gmail.com>
2026-05-26 23:30:36 +0200

    decimal in totals boxscores

--- game_results/game_results.js
+++ game_results/game_results.js
@@ -1,6 +1,6 @@
 import {
   game_results_array   
-} from '../data260526_2325.js';
+} from '../data260526_2330.js';
 
 const teamClassMap = {
   'SUD': 'score_sud',
--- team_rankings_div/team_rankings.js
+++ team_rankings_div/team_rankings.js
@@ -2,7 +2,7 @@ import {
     fantateam_type,
     NORD, SUD, EST, WEST,
     fantateams
-} from '../data260526_2325.js';
+} from '../data260526_2330.js';
 
 document.addEventListener('DOMContentLoaded', function() {
     const teamCardsContainer = document.getElementById('teamCardsContainer');
