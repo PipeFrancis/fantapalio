05fea0d32f231b808097c009fa849e6a2a82ad97
pipefrancis <francesco.tubaro@gmail.com>
2026-06-02 01:30:43 +0200

    rimesso tabella finale

--- coppachiosco/coppachiosco.js
+++ coppachiosco/coppachiosco.js
@@ -1,4 +1,4 @@
-import { rioni25 } from '../data260602_0123.js';
+import { rioni25 } from '../data260602_0130.js';
 
 window.addEventListener('DOMContentLoaded', () => {
     const container = document.querySelector('.team_detail_team-cards-container1');
--- meme_p_rankings/meme_p_rankings.js
+++ meme_p_rankings/meme_p_rankings.js
@@ -4,7 +4,7 @@ import {
     players,
     td3Weights,
     what_day_is_it
-} from '../data260602_0123.js';
+} from '../data260602_0130.js';
 
 document.addEventListener('DOMContentLoaded', function() {
     const playerCardsContainer = document.getElementById('playerCardsContainer');
