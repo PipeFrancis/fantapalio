d5d03a630151c282cc5e9005f8df5723bd83c012
pipefrancis <francesco.tubaro@gmail.com>
2026-06-02 22:23:39 +0200

    gruppobello fix

--- player_detail/player_detail.js
+++ player_detail/player_detail.js
@@ -3,7 +3,7 @@
 // se si cambiano array di stats è da cambiare anche questo codice
 
 
-import { players, pdkWeights, td3Weights, what_day_is_it, td3_bonus_passadaprimoultimo } from '../data260602_2205.js';
+import { players, pdkWeights, td3Weights, what_day_is_it, td3_bonus_passadaprimoultimo } from '../data260602_2223.js';
 
 document.addEventListener("DOMContentLoaded", function() {
     const select = document.getElementById("playerDetails");
--- registration/registration.js
+++ registration/registration.js
@@ -1,4 +1,4 @@
-import { players } from '../data260602_2205.js';
+import { players } from '../data260602_2223.js';
 console.log("inizio registration.js");
 
 // Group players by their team
