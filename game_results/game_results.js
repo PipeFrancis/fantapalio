77aa3abedad5c85992c3ba143f83278c868a4805
pipefrancis <francesco.tubaro@gmail.com>
2026-06-27 13:11:00 +0200

    tolto console log da avg

--- game_results/game_results.js
+++ game_results/game_results.js
@@ -1,6 +1,6 @@
 import {
   game_results_array   
-} from '../data260627_1308.js';
+} from '../data260627_1310.js';
 
 const teamClassMap = {
   'SUD': 'score_sud',
--- registration/registration.js
+++ registration/registration.js
@@ -1,4 +1,4 @@
-import { players } from '../data260627_1308.js';
+import { players } from '../data260627_1310.js';
 console.log("inizio registration.js");
 
 // Group players by their team
