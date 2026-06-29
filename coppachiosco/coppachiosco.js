12c9636d67fd001e8d2db3863691c058b13736ee
pipefrancis <francesco.tubaro@gmail.com>
2026-06-30 00:23:45 +0200

    GioZ spaces

--- coppachiosco/coppachiosco.js
+++ coppachiosco/coppachiosco.js
@@ -1,4 +1,4 @@
-import { rioni25 } from '../data260630_0021.js';
+import { rioni25 } from '../data260630_0023.js';
 
 window.addEventListener('DOMContentLoaded', () => {
     const container = document.querySelector('.team_detail_team-cards-container1');
--- game_results/game_results.js
+++ game_results/game_results.js
@@ -1,6 +1,6 @@
 import {
   game_results_array   
-} from '../data260630_0021.js';
+} from '../data260630_0023.js';
 
 const teamClassMap = {
   'SUD': 'score_sud',
