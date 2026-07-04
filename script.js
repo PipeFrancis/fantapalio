c6e72b8665a4b1f6e66f51cfca0f7aeef42c2f04
pipefrancis <francesco.tubaro@gmail.com>
2026-07-04 15:52:35 +0200

    30px threshold

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-import { fantateams , what_day_is_it, coachWeights} from './data260704_1549.js';
+import { fantateams , what_day_is_it, coachWeights} from './data260704_1552.js';
 
 document.addEventListener("DOMContentLoaded", function() {
     const select = document.getElementById("teamDetails");
--- team_rankings_div/team_rankings.js
+++ team_rankings_div/team_rankings.js
@@ -2,7 +2,7 @@ import {
     fantateam_type,
     NORD, SUD, EST, WEST,
     fantateams
-} from '../data260704_1549.js';
+} from '../data260704_1552.js';
 
 document.addEventListener('DOMContentLoaded', function() {
     const teamCardsContainer = document.getElementById('teamCardsContainer');
