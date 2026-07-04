f6d1730e064d1dedfdaff7f7fe16f2636c54c802
pipefrancis <francesco.tubaro@gmail.com>
2026-07-04 12:42:01 +0200

    style

--- registration/registration.js
+++ registration/registration.js
@@ -1,4 +1,4 @@
-import { players } from '../data260704_1136.js';
+import { players } from '../data260704_1241.js';
 console.log("inizio registration.js");
 
 // Group players by their team
--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-import { fantateams , what_day_is_it, coachWeights} from './data260704_1136.js';
+import { fantateams , what_day_is_it, coachWeights} from './data260704_1241.js';
 
 document.addEventListener("DOMContentLoaded", function() {
     const select = document.getElementById("teamDetails");
