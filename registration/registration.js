0622417551747eda51c8013d2b4fd3053ba194cd
pipefrancis <francesco.tubaro@gmail.com>
2026-07-04 16:20:01 +0200

    actual border under scrollbar

--- registration/registration.js
+++ registration/registration.js
@@ -1,4 +1,4 @@
-import { players } from '../data260704_1616.js';
+import { players } from '../data260704_1619.js';
 console.log("inizio registration.js");
 
 // Group players by their team
--- team_rankings_div/team_rankings.js
+++ team_rankings_div/team_rankings.js
@@ -2,7 +2,7 @@ import {
     fantateam_type,
     NORD, SUD, EST, WEST,
     fantateams
-} from '../data260704_1616.js';
+} from '../data260704_1619.js';
 
 document.addEventListener('DOMContentLoaded', function() {
     const teamCardsContainer = document.getElementById('teamCardsContainer');
