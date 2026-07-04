cd928d77f17619d631981fdeb3357e47b8ab40fb
pipefrancis <francesco.tubaro@gmail.com>
2026-07-04 16:40:24 +0200

    8px

--- game_results/game_results.js
+++ game_results/game_results.js
@@ -1,6 +1,6 @@
 import {
   game_results_array   
-} from '../data260704_1639.js';
+} from '../data260704_1640.js';
 
 const teamClassMap = {
   'SUD': 'score_sud',
--- t_rankings_by_league/t_rankings_by_league.js
+++ t_rankings_by_league/t_rankings_by_league.js
@@ -2,7 +2,7 @@ import {
     fantateam_type,
     NORD, SUD, EST, WEST,
     fantateams
-} from '../data260704_1639.js';
+} from '../data260704_1640.js';
 
 document.addEventListener('DOMContentLoaded', function() {
 
