42996123c1c6b1248900b1e019f545000a39f336
pipefrancis <francesco.tubaro@gmail.com>
2026-07-04 14:58:44 +0200

    onpointercancel empty

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-import { fantateams , what_day_is_it, coachWeights} from './data260704_1433.js';
+import { fantateams , what_day_is_it, coachWeights} from './data260704_1458.js';
 
 document.addEventListener("DOMContentLoaded", function() {
     const select = document.getElementById("teamDetails");
