3ad13d19126fbded263db6c228d27d9dbb25a99b
pipefrancis <francesco.tubaro@gmail.com>
2026-07-06 21:50:40 +0200

    pre iscrizioni ufficiale

--- registration/registration.js
+++ registration/registration.js
@@ -1,4 +1,4 @@
-import { players } from '../data260706_1932.js';
+import { players } from '../data260706_2150.js';
 console.log("inizio registration.js");
 
 // Group players by their team
--- t_rankings_by_game/t_rankings_by_game.js
+++ t_rankings_by_game/t_rankings_by_game.js
@@ -2,7 +2,7 @@ import {
     fantateam_type,
     NORD, SUD, EST, WEST,
     fantateams
-} from '../data260706_1932.js';
+} from '../data260706_2150.js';
 
 document.addEventListener('DOMContentLoaded', function() {
     const teamCardsContainer = document.getElementById('teamCardsContainer');
