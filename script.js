a0bb8295137e79ff2d6ceb2b9ef005c6a5e70364
pipefrancis <francesco.tubaro@gmail.com>
2026-07-06 08:50:29 +0200

    con il miglior punteggio

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-import { fantateams , what_day_is_it, coachWeights} from './data260706_0840.js';
+import { fantateams , what_day_is_it, coachWeights} from './data260706_0850.js';
 
 document.addEventListener("DOMContentLoaded", function() {
     const select = document.getElementById("teamDetails");
--- t_rankings_by_game/t_rankings_by_game.js
+++ t_rankings_by_game/t_rankings_by_game.js
@@ -2,7 +2,7 @@ import {
     fantateam_type,
     NORD, SUD, EST, WEST,
     fantateams
-} from '../data260706_0840.js';
+} from '../data260706_0850.js';
 
 document.addEventListener('DOMContentLoaded', function() {
     const teamCardsContainer = document.getElementById('teamCardsContainer');
