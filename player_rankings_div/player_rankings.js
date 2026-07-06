b4fe31f8b6201fe2d6bddcafb09fe83a1b57567d
pipefrancis <francesco.tubaro@gmail.com>
2026-07-06 19:22:34 +0200

    rules

--- player_rankings_div/player_rankings.js
+++ player_rankings_div/player_rankings.js
@@ -2,7 +2,7 @@ import {
     player_type,
     players,
     what_day_is_it
-} from '../data260706_0854.js';
+} from '../data260706_1922.js';
 
 document.addEventListener('DOMContentLoaded', function() {
     const playerCardsContainer = document.getElementById('playerCardsContainer');
--- registration/registration.js
+++ registration/registration.js
@@ -1,4 +1,4 @@
-import { players } from '../data260706_0854.js';
+import { players } from '../data260706_1922.js';
 console.log("inizio registration.js");
 
 // Group players by their team
