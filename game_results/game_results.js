872483b009153bea8a2503ffd9b79fcdfb5e3dc5
pipefrancis <francesco.tubaro@gmail.com>
2026-07-09 08:19:59 +0200

    emtpy fantateams array

--- game_results/game_results.js
+++ game_results/game_results.js
@@ -1,6 +1,6 @@
 import {
   game_results_array   
-} from '../data260709_0050.js';
+} from '../data260709_0819.js';
 
 const teamClassMap = {
   'SUD': 'score_sud',
--- registration/registration.js
+++ registration/registration.js
@@ -1,4 +1,4 @@
-import { players } from '../data260709_0050.js';
+import { players } from '../data260709_0819.js';
 console.log("inizio registration.js");
 
 // Group players by their team
