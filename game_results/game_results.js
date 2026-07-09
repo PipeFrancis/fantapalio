5466f901fd0161dd522b35730d6fd1b16d1f5caf
pipefrancis <francesco.tubaro@gmail.com>
2026-07-09 08:50:31 +0200

    html

--- game_results/game_results.js
+++ game_results/game_results.js
@@ -1,6 +1,6 @@
 import {
   game_results_array   
-} from '../data260709_0843.js';
+} from '../data260709_0850.js';
 
 const teamClassMap = {
   'SUD': 'score_sud',
--- player_rankings_div/player_rankings.js
+++ player_rankings_div/player_rankings.js
@@ -2,7 +2,7 @@ import {
     player_type,
     players,
     what_day_is_it
-} from '../data260709_0843.js';
+} from '../data260709_0850.js';
 
 document.addEventListener('DOMContentLoaded', function() {
     const playerCardsContainer = document.getElementById('playerCardsContainer');
