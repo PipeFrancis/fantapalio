2286cd1e285ef19193bc787162ee1b16e86fd0e8
pipefrancis <francesco.tubaro@gmail.com>
2026-07-11 00:43:11 +0200

    pull official

--- game_results/game_results.js
+++ game_results/game_results.js
@@ -1,6 +1,6 @@
 import {
   game_results_array   
-} from '../data260710_1956.js';
+} from '../data260711_0043.js';
 
 const teamClassMap = {
   'SUD': 'score_sud',
--- team_rankings_div/team_rankings.js
+++ team_rankings_div/team_rankings.js
@@ -2,7 +2,7 @@ import {
     fantateam_type,
     NORD, SUD, EST, WEST,
     fantateams
-} from '../data260710_1956.js';
+} from '../data260711_0043.js';
 
 document.addEventListener('DOMContentLoaded', function() {
     const teamCardsContainer = document.getElementById('teamCardsContainer');
