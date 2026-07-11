05d75936a22270ee9ff540fa2dac495956a6a6e7
pipefrancis <francesco.tubaro@gmail.com>
2026-07-11 19:11:25 +0200

    more teams

--- coppachiosco/coppachiosco.js
+++ coppachiosco/coppachiosco.js
@@ -1,4 +1,4 @@
-import { rioni25 } from '../data260711_0133.js';
+import { rioni25 } from '../data260711_1911.js';
 
 window.addEventListener('DOMContentLoaded', () => {
     const container = document.querySelector('.team_detail_team-cards-container1');
