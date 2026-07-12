51bfb90a27501dd400fabc2d0f4c24c4fa8db2d9
pipefrancis <francesco.tubaro@gmail.com>
2026-07-12 10:57:55 +0200

    html

--- game_results/game_results.js
+++ game_results/game_results.js
@@ -1,6 +1,6 @@
 import {
   game_results_array   
-} from '../data260712_1056.js';
+} from '../data260712_1057.js';
 
 const teamClassMap = {
   'SUD': 'score_sud',
--- registration/registration.js
+++ registration/registration.js
@@ -1,4 +1,4 @@
-import { players } from '../data260712_1056.js';
+import { players } from '../data260712_1057.js';
 console.log("inizio registration.js");
 
 // Group players by their team
