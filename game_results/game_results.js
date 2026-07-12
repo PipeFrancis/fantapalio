6f4ed5b5dab708a6be870b9d7fad4afd8e3f277b
pipefrancis <francesco.tubaro@gmail.com>
2026-07-12 13:31:27 +0200

    images

--- game_results/game_results.js
+++ game_results/game_results.js
@@ -1,6 +1,6 @@
 import {
   game_results_array   
-} from '../data260712_1109.js';
+} from '../data260712_1331.js';
 
 const teamClassMap = {
   'SUD': 'score_sud',
--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-import { fantateams , what_day_is_it, coachWeights} from './data260712_1109.js';
+import { fantateams , what_day_is_it, coachWeights} from './data260712_1331.js';
 
 document.addEventListener("DOMContentLoaded", function() {
     const select = document.getElementById("teamDetails");
