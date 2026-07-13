4a479b73ee2527df41ef80d1a366cc0ff0e2401d
pipefrancis <francesco.tubaro@gmail.com>
2026-07-13 03:28:32 +0200

    no print t3p

--- registration/registration.js
+++ registration/registration.js
@@ -1,4 +1,4 @@
-import { players } from '../data260713_0326.js';
+import { players } from '../data260713_0328.js';
 console.log("inizio registration.js");
 
 // Group players by their team
--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-import { fantateams , what_day_is_it, coachWeights} from './data260713_0326.js';
+import { fantateams , what_day_is_it, coachWeights} from './data260713_0328.js';
 
 document.addEventListener("DOMContentLoaded", function() {
     const select = document.getElementById("teamDetails");
