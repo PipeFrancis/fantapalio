37b908f4702d384fc135dfd8edc5f28ad1a24133
pipefrancis <francesco.tubaro@gmail.com>
2026-07-13 03:22:52 +0200

    hmtl

--- team_rankings_div/team_rankings.js
+++ team_rankings_div/team_rankings.js
@@ -2,7 +2,7 @@ import {
     fantateam_type,
     NORD, SUD, EST, WEST,
     fantateams
-} from '../data260713_0321.js';
+} from '../data260713_0322.js';
 
 document.addEventListener('DOMContentLoaded', function() {
     const teamCardsContainer = document.getElementById('teamCardsContainer');
