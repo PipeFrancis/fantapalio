266b4f1a3c4602b2c7f55b207e1a522d2602efc8
pipefrancis <francesco.tubaro@gmail.com>
2026-07-14 02:05:10 +0200

    game results orde

--- coppachiosco/coppachiosco.js
+++ coppachiosco/coppachiosco.js
@@ -1,4 +1,4 @@
-import { rioni25 } from '../data260714_0203.js';
+import { rioni25 } from '../data260714_0205.js';
 
 window.addEventListener('DOMContentLoaded', () => {
     const container = document.querySelector('.team_detail_team-cards-container1');
--- game_results/game_results.js
+++ game_results/game_results.js
@@ -1,6 +1,6 @@
 import {
   game_results_array   
-} from '../data260714_0203.js';
+} from '../data260714_0205.js';
 
 const teamClassMap = {
   'SUD': 'score_sud',
