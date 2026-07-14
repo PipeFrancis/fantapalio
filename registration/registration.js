88a9fdb85c1387d624007193be8b61a55cc4de32
pipefrancis <francesco.tubaro@gmail.com>
2026-07-14 02:22:14 +0200

    fix stat

--- registration/registration.js
+++ registration/registration.js
@@ -1,4 +1,4 @@
-import { players } from '../data260714_0206.js';
+import { players } from '../data260714_0222.js';
 console.log("inizio registration.js");
 
 // Group players by their team
--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-import { fantateams , what_day_is_it, coachWeights} from './data260714_0206.js';
+import { fantateams , what_day_is_it, coachWeights} from './data260714_0222.js';
 
 document.addEventListener("DOMContentLoaded", function() {
     const select = document.getElementById("teamDetails");
