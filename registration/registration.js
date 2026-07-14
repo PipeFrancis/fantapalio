f2a21288d73c56ef15aca48f05cee8ec0a028a92
pipefrancis <francesco.tubaro@gmail.com>
2026-07-14 20:10:49 +0200

    2 birre iob e occhiali comi

--- registration/registration.js
+++ registration/registration.js
@@ -1,4 +1,4 @@
-import { players } from '../data260714_1727.js';
+import { players } from '../data260714_2010.js';
 console.log("inizio registration.js");
 
 // Group players by their team
--- t_rankings_by_league/t_rankings_by_league.js
+++ t_rankings_by_league/t_rankings_by_league.js
@@ -2,7 +2,7 @@ import {
     fantateam_type,
     NORD, SUD, EST, WEST,
     fantateams
-} from '../data260714_1727.js';
+} from '../data260714_2010.js';
 
 document.addEventListener('DOMContentLoaded', function() {
 
