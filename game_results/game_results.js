f2363fd72321737aa5175390123cfaf96bd0ccca
pipefrancis <francesco.tubaro@gmail.com>
2026-07-15 00:10:23 +0200

    coppa chiosco rioni

--- game_results/game_results.js
+++ game_results/game_results.js
@@ -1,6 +1,6 @@
 import {
   game_results_array   
-} from '../data260715_0007.js';
+} from '../data260715_0010.js';
 
 const teamClassMap = {
   'SUD': 'score_sud',
--- registration/registration.js
+++ registration/registration.js
@@ -1,4 +1,4 @@
-import { players } from '../data260715_0007.js';
+import { players } from '../data260715_0010.js';
 console.log("inizio registration.js");
 
 // Group players by their team
