1dff49369af9c6a48844eb9a8938f451bb454b5f
pipefrancis <francesco.tubaro@gmail.com>
2026-07-14 16:02:46 +0200

    micottis e bravin meme fixati

--- t_rankings_by_league/t_rankings_by_league.js
+++ t_rankings_by_league/t_rankings_by_league.js
@@ -2,7 +2,7 @@ import {
     fantateam_type,
     NORD, SUD, EST, WEST,
     fantateams
-} from '../data260714_1545.js';
+} from '../data260714_1602.js';
 
 document.addEventListener('DOMContentLoaded', function() {
 
