49dad1115afba2a78842f0b6342f437f515884bf
pipefrancis <francesco.tubaro@gmail.com>
2026-07-14 15:12:59 +0200

    no ballerina fedrizzi

--- player_rankings_div/player_rankings.js
+++ player_rankings_div/player_rankings.js
@@ -2,7 +2,7 @@ import {
     player_type,
     players,
     what_day_is_it
-} from '../data260714_1501.js';
+} from '../data260714_1512.js';
 
 document.addEventListener('DOMContentLoaded', function() {
     const playerCardsContainer = document.getElementById('playerCardsContainer');
--- registration/registration.js
+++ registration/registration.js
@@ -1,4 +1,4 @@
-import { players } from '../data260714_1501.js';
+import { players } from '../data260714_1512.js';
 console.log("inizio registration.js");
 
 // Group players by their team
