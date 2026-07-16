ebe8952136563e091429b2e8e9171647b1512a18
pipefrancis <francesco.tubaro@gmail.com>
2026-07-16 13:21:15 +0200

    mattia collavini no minorenne g2

--- player_rankings_div/player_rankings.js
+++ player_rankings_div/player_rankings.js
@@ -2,7 +2,7 @@ import {
     player_type,
     players,
     what_day_is_it
-} from '../data260716_1320.js';
+} from '../data260716_1321.js';
 
 document.addEventListener('DOMContentLoaded', function() {
     const playerCardsContainer = document.getElementById('playerCardsContainer');
