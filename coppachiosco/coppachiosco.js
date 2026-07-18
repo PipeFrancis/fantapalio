80124384ffd44f8db9191f5e15a6951a7fe82f8e
pipefrancis <francesco.tubaro@gmail.com>
2026-07-18 11:14:39 +0200

    spanolo pasquon ciabatte

--- coppachiosco/coppachiosco.js
+++ coppachiosco/coppachiosco.js
@@ -1,4 +1,4 @@
-import { rioni } from '../data260718_1052.js';
+import { rioni } from '../data260718_1114.js';
 
 window.addEventListener('DOMContentLoaded', () => {
     const container = document.querySelector('.team_detail_team-cards-container1');
--- game_results/game_results.js
+++ game_results/game_results.js
@@ -1,6 +1,6 @@
 import {
   game_results_array   
-} from '../data260718_1052.js';
+} from '../data260718_1114.js';
 
 const teamClassMap = {
   'SUD': 'score_sud',
