0f5ba7af764fbfaff86901d4e3a428e2dedc2002
pipefrancis <francesco.tubaro@gmail.com>
2026-07-30 23:24:31 +0200

    birre plurale anche per coach

--- game_results/game_results.js
+++ game_results/game_results.js
@@ -1,6 +1,6 @@
 import {
   game_results_array   
-} from '../data260730_2323.js';
+} from '../data260730_2324.js';
 
 const teamClassMap = {
   'SUD': 'score_sud',
--- t_rankings_by_league/t_rankings_by_league.js
+++ t_rankings_by_league/t_rankings_by_league.js
@@ -2,7 +2,7 @@ import {
     fantateam_type,
     NORD, SUD, EST, WEST,
     fantateams
-} from '../data260730_2323.js';
+} from '../data260730_2324.js';
 
 document.addEventListener('DOMContentLoaded', function() {
 
