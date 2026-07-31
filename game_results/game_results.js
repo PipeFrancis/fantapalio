622731b382d8cf370bed6c74e56cafb445fe3897
pipefrancis <francesco.tubaro@gmail.com>
2026-07-31 19:10:45 +0200

    table with class

--- game_results/game_results.js
+++ game_results/game_results.js
@@ -1,6 +1,6 @@
 import {
   game_results_array   
-} from '../data260731_1904.js';
+} from '../data260731_1910.js';
 
 const teamClassMap = {
   'SUD': 'score_sud',
--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-import { fantateams , what_day_is_it, coachWeights} from './data260731_1904.js';
+import { fantateams , what_day_is_it, coachWeights} from './data260731_1910.js';
 
 document.addEventListener("DOMContentLoaded", function() {
     const select = document.getElementById("teamDetails");
