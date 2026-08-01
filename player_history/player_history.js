6c6a7d97e9472658af2a274163085ca207fb7c57
pipefrancis <francesco.tubaro@gmail.com>
2026-08-01 14:28:37 +0200

    timestamp

--- player_history/player_history.js
+++ player_history/player_history.js
@@ -32,7 +32,7 @@ import { groupedPlayersHistoricalData,
         TD3_0SU10          ,
         TD3_CIAB           ,
         TD3_ALTRI_MEME     ,
-} from '../data260801_0209.js';
+} from '../data260801_1428.js';
 
 // Global variable to keep track of the Chart instance
 // let chartInstance = null;
