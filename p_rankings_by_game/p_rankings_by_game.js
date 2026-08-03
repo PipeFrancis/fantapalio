fb032b9df13a560c628a6c42eae6ad0537c00eaa
pipefrancis <francesco.tubaro@gmail.com>
2026-08-03 21:11:03 +0200

    not sort by cost

--- p_rankings_by_game/p_rankings_by_game.js
+++ p_rankings_by_game/p_rankings_by_game.js
@@ -32,7 +32,7 @@ import {
     TD3_0SU10          ,
     TD3_CIAB           ,
     TD3_ALTRI_MEME     ,
-} from '../data260803_2107.js';
+} from '../data260803_2110.js';
 
 document.addEventListener('DOMContentLoaded', () => {
     const playerCardsContainer = document.getElementById('playerCardsContainer');
--- t_rankings_by_league/t_rankings_by_league.js
+++ t_rankings_by_league/t_rankings_by_league.js
@@ -2,7 +2,7 @@ import {
     fantateam_type,
     NORD, SUD, EST, WEST,
     fantateams
-} from '../data260803_2107.js';
+} from '../data260803_2110.js';
 
 document.addEventListener('DOMContentLoaded', function() {
 
